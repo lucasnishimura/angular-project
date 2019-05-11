72b9c5d209b8d39556361bddfa6d9cac51103525
lucasnishimura <32456478+lucasnishimura@users.noreply.github.com>
2019-05-11 19:08:39 -0300

    add servidores condicionais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nome = 'Lucas';
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer(){
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    })
+  }
+
+  onAddServerBlueprint(){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    })
+  }
 }
