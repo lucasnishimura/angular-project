a818e82329c998291c1c2b59b528b487cb78f7d3
lucasnishimura <32456478+lucasnishimura@users.noreply.github.com>
2019-05-16 08:22:35 -0300

    local references

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,21 +9,25 @@ export class CockpitComponent implements OnInit {
   //o outoput serve para indicar que os dados daqui processarão em outro lugar, indica a variável que vai enviar eo tipo dela
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   onAddServer(nameInput: HTMLInputElement){
     this.serverCreated.emit({
      serverName: nameInput.value,
-     serverContent: this.newServerContent
+     serverContent: this.serverContentInput.nativeElement.value
+     //  serverContent: this.newServerContent
     })
   }
-
-  onAddServerBlueprint(){
+  
+  onAddServerBlueprint(nameInput: HTMLInputElement){
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
-     })
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+      // serverName: this.newServerName,
+      // serverContent: this.newServerContent
+      })
   }
 
   constructor() { }
