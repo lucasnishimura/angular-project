4ae2913b7fd8dfc07f53d0fbf642598d67866e32
lucasnishimura <32456478+lucasnishimura@users.noreply.github.com>
2019-06-07 08:51:49 -0300

    criando funções

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../loggin.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService] //usado para adicionar métodos e usa-lo neste component
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private logginService: LoggingService){} //usado para instanciar o nossa classe com o método que preciso
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.logginService.logStatusChange(status)  //chama o método como se ele estivesse no nosso escopo
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,24 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../loggin.service';
+
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService] //usado para adicionar métodos e usa-lo neste component
 })
+
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private logginService: LoggingService){} //usado para instanciar o nossa classe com o método que preciso
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.logginService.logStatusChange(accountStatus)  //chama o método como se ele estivesse no nosso escopo
   }
 }
