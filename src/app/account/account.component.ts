ab1950ae3eabbbf71c5d2d2fbfa35845c6bf0dfc
lucasnishimura <32456478+lucasnishimura@users.noreply.github.com>
2019-06-10 08:00:38 -0300

    function de alimentar o front com os dados, porém ainda com erro de acordo com a aula

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,21 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LoggingService } from '../loggin.service';
+import { AccountsService } from '../account.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService] //usado para adicionar métodos e usa-lo neste component
+  providers: [LoggingService, AccountsService] //usado para adicionar métodos e usa-lo neste component
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private logginService: LoggingService){} //usado para instanciar o nossa classe com o método que preciso
+  constructor(private logginService: LoggingService,
+  private accountsService: AccountsService){} //usado para instanciar o nossa classe com o método que preciso
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus(this.id,status)
     this.logginService.logStatusChange(status)  //chama o método como se ele estivesse no nosso escopo
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,24 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../loggin.service';
+import { AccountsService } from '../account.service';
 
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService] //usado para adicionar métodos e usa-lo neste component
+  providers: [LoggingService, AccountsService] //usado para adicionar métodos e usa-lo neste component
 })
 
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
-  constructor(private logginService: LoggingService){} //usado para instanciar o nossa classe com o método que preciso
+  constructor(private logginService: LoggingService,
+  private accountsService: AccountsService){} //usado para instanciar o nossa classe com o método que preciso
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountsService.addAccount(accountName, accountStatus);
     this.logginService.logStatusChange(accountStatus)  //chama o método como se ele estivesse no nosso escopo
   }
 }
