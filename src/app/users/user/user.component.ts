f8bf7294f1794f2d4bb40594223da362ee83546e
lucasnishimura <32456478+lucasnishimura@users.noreply.github.com>
2019-06-25 08:13:15 -0300

    rotas dinâmicas e carregar rota específica

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -14,8 +14,18 @@ export class UserComponent implements OnInit {
   ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'],
-      name: 'Lucas'
+      name: this.route.snapshot.params['name']
     }
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
+
+  
 
 }
